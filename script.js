bc4f62a6359443861853b6083a70aa330b67ba27
Jasmine Kay <jasminekay505@gmail.com>
2021-01-20 13:54:21 -0800

    Setting up  timer

---
+++ script.js
@@ -0,0 +1,46 @@
+//Set up array with question objects
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ___.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    }
+];
+
+
+//Timer
+var timer = document.querySelector("#timer");
+var startTimer = document.querySelector("#submit");
+var secondsLeft = 60;
+var pentalty = 10;
+
+
+startTimer.addEventListener("click", function () { 
+    secondsLeft--;
+    timer.textContent = "Time: " + secondsLeft;
+    if (secondsLeft <=0) { 
+        timer.textContent = "Time's Up!";
+    }
+}, 1000);
+function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
+    
+    function showQuestions (questions, quizContainer) { 
+        //Clear data
+        questionsDiv.innerHTML = "";
+        ul
+    }
+    function showResults (questions, quizContainer, resultsContainer) {
+
+    }
+
+    showQuestions(questions, quizContainer);
+
+    submitButton.onclick = function () {
+        showResults(questions, quizContainer, resultsContainer);
+    }
+}
